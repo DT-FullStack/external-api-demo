f24eb2391ca8b658bb493d534661c060698f6910
bodywizard <david@bodywizardmedicine.com>
2021-12-02 19:05:09 -0600

    Toggle Project Highlights

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 import AppLink from './layout/AppLink'
 
 const Home = () => {
@@ -15,11 +15,17 @@ const Home = () => {
   const addLabelEmphasis = ({ target }) => { target.classList.add('hover'); target.classList.add('red'); }
   const removeLabelEmphasis = ({ target }) => { target.classList.remove('hover'); target.classList.remove('red'); }
 
+  const [showingDetails, setShowingDetails] = useState(false);
+  const arrowClass = () => showingDetails ? 'angle right spin90 icon' : 'angle right icon';
+  const toggleClass = () => showingDetails ? 'toggle active' : 'toggle';
+  const toggle = () => setShowingDetails(!showingDetails);
+
   return (
     <Fragment>
       <h1 className='ui green header'>Utilizing External APIs</h1>
       <h2 className='ui header'>This simple project connects to three external APIs to demonstrate knowledge of how to use endpoints and use the resulting data.</h2>
-      <div className="ui basic segment">
+
+      <div id="APIS" className="ui basic segment">
         <div className="ui three cards">
           <div className="ui green card">
             <div className="content">
@@ -64,53 +70,61 @@ const Home = () => {
 
       </div>
       <div className="ui divider"></div>
-      <div className="ui basic segment">
-        <h3 className='ui red header'>Project Details</h3>
 
-        <div className="ui three cards">
-          <div className="ui raised red card">
-            <div className="content">
-              <div className="header">Convenient UI</div>
-              <div className="description">Searching and selecting results is made easy, and includes external links</div>
-            </div>
-          </div>
-          <div className="ui raised red card">
-            <div className="content">
-              <div className="header">Minimized API Calls</div>
-              <div className="description">
-                <div className="ui list">
-                  <div className="item">Search-as-you-type functionality</div>
-                  <div className="item">Carried out after the user stops typing</div>
-                  <div className="item">Cached (memoized) to prevent duplicate search requests</div>
+      <div id="HIGHLIGHTS" className="ui basic segment">
+        <h3 className='ui red header' onClick={toggle}><i className={arrowClass()}></i>Project Highlights</h3>
+        {showingDetails && (
+          <Fragment>
+            <div className="ui three cards">
+              <div className="ui raised red card">
+                <div className="content">
+                  <div className="header">Convenient UI</div>
+                  <div className="description">Searching and selecting results is made easy, and includes external links</div>
+                </div>
+              </div>
+              <div className="ui raised red card">
+                <div className="content">
+                  <div className="header">Minimized API Calls</div>
+                  <div className="description">
+                    <div className="ui list">
+                      <div className="item">Search-as-you-type functionality</div>
+                      <div className="item">Carried out after the user stops typing</div>
+                      <div className="item">Cached (memoized) to prevent duplicate search requests</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="ui raised red card">
+                <div className="content">
+                  <div className="header">State History</div>
+                  <div className="description">
+                    <div className="ui list">
+                      <div className="item">Search results can be selected for more information</div>
+                      <div className="item">Selected results are stored</div>
+                      <div className="item">History of selected items is navigable</div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="ui raised red card">
-            <div className="content">
-              <div className="header">State History</div>
-              <div className="description">
-                <div className="ui list">
-                  <div className="item">Search results can be selected for more information</div>
-                  <div className="item">Selected results are stored</div>
-                  <div className="item">History of selected items is navigable</div>
-                </div>
-              </div>
+            <div className="ui basic center aligned segment">
+              {
+                Object.keys(packages).map(key => (
+                  <div key={key} className="ui large label" onMouseEnter={addLabelEmphasis} onMouseLeave={removeLabelEmphasis}>
+                    {key}
+                    <div className="detail">{packages[key]}</div>
+                  </div>
+                ))
+              }
+
             </div>
-          </div>
+          </Fragment>
+        )}
+        <div className={toggleClass()}>
+
         </div>
       </div>
-      <div className="ui basic center aligned segment">
-        {
-          Object.keys(packages).map(key => (
-            <div key={key} className="ui large label" onMouseEnter={addLabelEmphasis} onMouseLeave={removeLabelEmphasis}>
-              {key}
-              <div className="detail">{packages[key]}</div>
-            </div>
-          ))
-        }
 
-      </div>
 
 
 
